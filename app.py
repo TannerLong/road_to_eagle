254b597fad035cf222a36a7c52c538522c71caf2
TannerLong <tannerweslong@gmail.com>
2026-05-03 15:16:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("Road to Eagle app is starting")
+scout_name = "Name: Harrison"
+current_rank = "rank: star scout"
+age = "age: 46"
+badges_earned = "Merit Badges: 12"
+is_eagle = False
+print(scout_name)
+print(current_rank)
+print(age)
+print(badges_earned)
